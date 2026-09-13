6900af1a22760e04d3d17f8243bcff3a9e923515
max <luizfvm31@gmail.com>
2026-09-13 20:08:46 -0300

    update

--- bruto.py
+++
@@ -1,6 +0,0 @@
-import urllib.request    # Download
-
-url = "ftp://ftp.datasus.gov.br/dissemin/publicos/SINASC/NOV/DOCS/Legislacao_PDF.pdf"
-nome = url.split('/')[-1]
-urllib.request.urlretrieve(url, nome)
-
